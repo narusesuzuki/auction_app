7b5fdb9f9e89c995df3fdf30ed73fe31e8ede9f2
“Naruse” <satoshisennsyu@gmail.com>
2020-10-10 17:24:05 +0900

    heroku設定

--- server/routes/web.php
+++ server/routes/web.php
@@ -16,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Route::get('/', 'ItemsController@index);
+Route::get('/','ItemController@index');
 Route::resource('items', 'ItemController');
