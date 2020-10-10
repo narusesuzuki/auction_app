55130ba4e3b18e9c5ab3cb3807b8d4afd7a69147
“Naruse” <satoshisennsyu@gmail.com>
2020-10-10 17:07:01 +0900

    first commit

---
+++ server/resources/views/items/index.blade.php
@@ -0,0 +1,9 @@
+<h1>出品商品一覧</h1>
+<ul>
+    @foreach ($items as $item)
+    <!-- // リンク先をidで取得し名前で出力 -->
+    <li><a href="/items/{{ $item->id }}">{{ $item->name }}</a></li>
+    @endforeach
+</ul>
+
+<a href="/items/create">出品する</a>
---
+++ server/resources/views/items/show.blade.php
@@ -0,0 +1,35 @@
+  <p>
+
+       <b>商品名：{{ $item->name }}</b>
+  </p>
+
+  <p>
+
+       <b>商品詳細：{{ $item->description }}</b>
+  </p>
+
+  <p>
+
+       <b>価格：{{ $item->price }}</b>
+  </p>
+
+  <p>
+
+       <b>出品者：{{ $item->seller }}</b>
+  </p>
+
+  <p>
+
+       <b>電子メール：{{ $item->email }}</b>
+  </p>
+
+  <p>
+       <img src="{{ $item->image_url }}">
+  </p>
+
+  <a href="/items/{{ $item->id }}/edit"><button>編集する</button></a>
+  <form action="/items/{{ $item->id }}" method="post">
+       @csrf
+       @method('DELETE')
+       <input type="submit" value="削除する" onclick="if(!confirm('削除しますか？')){return false};">
+  </form>
